957bc017f5ec5af65ba528f0bf993759a84b586c
haseebhasan2019 <haseebhasan2019@gmail.com>
2022-01-10 23:49:50 -0500

    Delete page formatting

--- delete.php
+++ delete.php
@@ -1,41 +1,35 @@
 <?php
     $con=mysqli_connect("localhost","root","","siddcutz");
     // Check connection
-    if (mysqli_connect_errno())
-    {
+    if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-    $id=$_GET['rn'];
-    echo $id;
-    
-    // Get time of appointment
-    $time = "none";
-    $getTime = mysqli_query($con, "SELECT `time` from appointment where `id` = $id");
-    while($row = mysqli_fetch_array($getTime))
-    {
-        $time = $row['time'];
-    }
-    echo $time;
-
-    // Change availability to available in database based on id of appointment
-    $sql = "UPDATE `availability` SET `$time`='1' WHERE `date`=(SELECT `date` from appointment where `id` = $id);";
-    echo $sql;
-    if ($con->query($sql) === TRUE) {
-        echo "Successfully updated availability. ";
     } else {
-        echo "Error updating availability: " . $con->error;
-    }
+        // Get time of appointment
+        $id=$_GET['rn'];
+        echo $id;
+        $time = "none";
+        $getTime = mysqli_query($con, "SELECT `time` from appointment where `id` = $id");
+        while ($row = mysqli_fetch_array($getTime)) {
+            $time = $row['time'];
+        }
+        echo $time;
 
-    //Delete appointment
-    $query = "DELETE FROM appointment where id='$id'";
-    $data=mysqli_query($con, $query);
-    if ($data)
-    {
-        echo "Record deleted from database."; //convert to alert?
-        header('Location: admin.php');
-    }
-    else
-    {
-        echo "Failed to delete record from database.";
+        // Change availability to available in database based on id of appointment
+        $sql = "UPDATE `availability` SET `$time`='1' WHERE `date`=(SELECT `date` from appointment where `id` = $id);";
+        echo $sql;
+        if ($con->query($sql) === TRUE)  {
+            echo "Successfully updated availability. ";
+            //Delete appointment
+            $query = "DELETE FROM appointment where id='$id'";
+            $data=mysqli_query($con, $query);
+            if ($data) {
+                echo "Record deleted from database."; //convert to alert?
+                header('Location: admin.php');
+            } else {
+                echo "Failed to delete record from database.";
+            }
+        } else {
+            echo "Error updating availability: " . $con->error;
+        }
     }
 ?>
